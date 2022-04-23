bae4e93a50d3a92e0e05d2935448d6d2f2c61f0f
kmjyz44 <kmjyz44@gmail.com>
2022-04-23 19:00:35 +0300

    burger_menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 let burger = document.querySelector("#burger_menu");
-let menu = document.querySelector(".menu")
+let menu = document.querySelector(".menu");
+let wrapper = document.querySelector(".wrapper");
+
 burger.addEventListener('click', function(){
     burger.classList.toggle('burger_menu_active');
     menu.classList.toggle('menu_active');  
+    wrapper.classList.toggle('wrapper_active');
 });
+
 
     //burger.classList.remove('burger_menu_active');  
 
