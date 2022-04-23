355a089714a65dc83ad04113319bcc779a7bc414
kmjyz44 <kmjyz44@gmail.com>
2022-04-23 19:58:11 +0300

    burger_menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
 let burger = document.querySelector("#burger_menu");
 let menu = document.querySelector(".menu");
 let wrapper = document.querySelector(".wrapper");
+let body=document.querySelector("body")
+
+
 
 burger.addEventListener('click', function(){
     burger.classList.toggle('burger_menu_active');
     menu.classList.toggle('menu_active');  
     wrapper.classList.toggle('wrapper_active');
 });
+    body.addEventListener('click', function(){
+        if(burger.classList.contains('burger_menu_active')){
+    menu.classList.toogle('menu_active');  
+    wrapper.classList.toogle('wrapper_active')
+        }
+})
 
 
     //burger.classList.remove('burger_menu_active');  
